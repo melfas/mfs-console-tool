98a43a82237356239f8a0bf06b4b9ca2470fa1ee
yjjung <yjjung@melfas.corp-partner.google.com>
2019-07-02 14:12:27 +0900

    version change

--- src/main.cpp
+++ src/main.cpp
@@ -437,7 +437,7 @@ int main(int argc, char* argv[])
 	if( nAction == ACTION_NULL || nAction == ACTION_HELP)
 	{
 		printf("===========================================================\n");
-		printf("melfas_update_tool for chromeOS (HID_USB) - v0.0.3\n\n");
+		printf("melfas_update_tool for chromeOS (HID_USB) - v0.0.4\n\n");
 		printf("ex) melfas_update_tool -fw_update pid fw_name\n");		
 		printf("ex) melfas_update_tool -fw_version pid\n");		
 		printf("===========================================================\n");
